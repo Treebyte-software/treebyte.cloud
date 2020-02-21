89351747dcdf0b870200ee22475f1b062389045b
Marco Stefani <57522146+marco-stefani-treebyte@users.noreply.github.com>
2020-02-21 14:43:58 +0100

    Update home-section-mobilepos.component.ts

--- src/app/features/home/components/home-section-mobilepos/home-section-mobilepos.component.ts
+++ src/app/features/home/components/home-section-mobilepos/home-section-mobilepos.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomeSectioMobileposComponent implements OnInit {
   title = 'POINT OF SALE';
   description2 = 'La soluzione per l'automazione del punto vendita che ti permette di gestire con semplicità tutte le attività e le operazioni all'interno del tuo negozio, del tuo ristorante e della tua catena commerciale.  ';
-  description3 = 'Una soluzione di front end in versione desktop e mobile, versatile dall''interfaccia semplice e con un elevata possibilità di personalizzazione.  ';
+  description3 = 'Una soluzione di front end in versione desktop e mobile, versatile dall'interfaccia semplice e con un elevata possibilità di personalizzazione.  ';
   titleBtn = 'Maggiori informazioni';
   constructor() { }
 

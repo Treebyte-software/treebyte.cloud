70990d3d703e4097c90daeaa0601275b0804e3e1
Marco Stefani <57522146+marco-stefani-treebyte@users.noreply.github.com>
2020-02-21 14:30:29 +0100

    Update home-section-mobilepos.component.ts

--- src/app/features/home/components/home-section-mobilepos/home-section-mobilepos.component.ts
+++ src/app/features/home/components/home-section-mobilepos/home-section-mobilepos.component.ts
@@ -6,8 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-section-mobilepos.component.css']
 })
 export class HomeSectioMobileposComponent implements OnInit {
-  title = 'MOBILE POS';
-  description2 = 'La soluzione progettata per il tuo Retail: in cloud, sempre disponibile, fruibile da qualsiasi dispositivo mobile e integrata con le più diffuse piattaforme ERP: Zucchetti, Microsoft, Oracle e SAP.  ';
+  title = 'POINT OF SALE';
+  description2 = 'La soluzione per l'automazione del punto vendita che ti permette di gestire con semplicità tutte le attività e le operazioni all'interno del tuo negozio, del tuo ristorante e della tua catena commerciale.  ';
+  description3 = 'Una soluzione di front end in versione desktop e mobile, versatile dall''interfaccia semplice e con un elevata possibilità di personalizzazione.  ';
   titleBtn = 'Maggiori informazioni';
   constructor() { }
 
